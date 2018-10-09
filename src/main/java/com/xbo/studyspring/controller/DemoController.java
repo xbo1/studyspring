94e4b2b3bb45e9ac7c5f873097798f16b940d52b
bob <wangshyn@gmail.com>
2018-10-09 17:54:23 +0800

    增加demoController 参考:https://blog.lqdev.cn/2018/07/11/springboot/chapter-one/

--- src/main/java/com/xbo/studyspring/controller/DemoController.java
+++ src/main/java/com/xbo/studyspring/controller/DemoController.java
@@ -1,13 +1,18 @@
 package com.xbo.studyspring.controller;
 
+import com.xbo.studyspring.model.Demo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class DemoController {
     @RequestMapping(value = "/demo", method = RequestMethod.GET)
     public String demo() {
+        Demo d = Demo.builder().code("123").name("bob").build();
+        log.warn(d.toString());
         return "hello xbo";
     }
 }
---
+++ src/main/java/com/xbo/studyspring/model/Demo.java
@@ -0,0 +1,12 @@
+package com.xbo.studyspring.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Demo {
+    String code;
+    String name;
+}
