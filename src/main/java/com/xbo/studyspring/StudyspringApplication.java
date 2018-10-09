eab930321ba01e34f32e5436e848e3bc3fbecc7c
bob <wangshyn@gmail.com>
2018-10-09 16:37:43 +0800

    init for spring boot add web module

---
+++ src/main/java/com/xbo/studyspring/StudyspringApplication.java
@@ -0,0 +1,12 @@
+package com.xbo.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyspringApplication.class, args);
+    }
+}
