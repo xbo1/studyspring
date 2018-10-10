aa56752dc79e7c3886767981a81f6ae752b423ea
bob <wangshyn@gmail.com>
2018-10-10 15:21:44 +0800

    统一异常、数据校验处理 参考:https://blog.lqdev.cn/2018/07/20/springboot/chapter-eight/

--- src/main/java/com/xbo/studyspring/controller/DemoController.java
+++ src/main/java/com/xbo/studyspring/controller/DemoController.java
@@ -1,17 +1,35 @@
 package com.xbo.studyspring.controller;
 
+import com.xbo.studyspring.exception.CommonException;
 import com.xbo.studyspring.model.Config;
 import com.xbo.studyspring.model.Demo;
+import com.xbo.studyspring.model.DemoReq;
+import com.xbo.studyspring.util.ValidatorUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @Slf4j
 public class DemoController {
+
+    @GetMapping("/err")
+    public String err() {
+        throw new CommonException("01001", "发送异常");
+    }
+
+    @GetMapping("/demo/valid")
+    public String demoValid(@Valid DemoReq req) {
+        //手动校验
+        ValidatorUtil.validate(req);
+        return req.getCode() + "," + req.getName();
+    }
     @RequestMapping(value = "/demo", method = RequestMethod.GET)
     public String demo() {
         Demo d = Demo.builder().code("123").name("bob").build();
