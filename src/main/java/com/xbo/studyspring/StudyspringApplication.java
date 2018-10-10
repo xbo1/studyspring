48453887f2ec916fb384fab394c7c52152c315f4
bob <wangshyn@gmail.com>
2018-10-10 14:22:26 +0800

    过滤器、监听器、拦截器 参考:https://blog.lqdev.cn/2018/07/19/springboot/chapter-seven/

--- src/main/java/com/xbo/studyspring/StudyspringApplication.java
+++ src/main/java/com/xbo/studyspring/StudyspringApplication.java
@@ -1,13 +1,43 @@
 package com.xbo.studyspring;
 
+import com.xbo.studyspring.config.CustomFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.servlet.Filter;
+
 @SpringBootApplication
+@ServletComponentScan //使用FilterRegistrationBean时注释掉
 @PropertySource(value="classpath:my.properties",encoding="utf-8")
+@Slf4j
 public class StudyspringApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudyspringApplication.class, args);
+        log.info("StudySpring 服务启动");
     }
+
+//    @Bean
+////    public FilterRegistrationBean filterRegistrationBean() {
+////        FilterRegistrationBean registration = new FilterRegistrationBean();
+////        //当过滤器有注入其他bean类时，可直接通过@bean的方式进行实体类过滤器，这样不可自动注入过滤器使用的其他bean类。
+////        //当然，若无其他bean需要获取时，可直接new CustomFilter()，也可使用getBean的方式。
+////        registration.setFilter(customFilter());
+////        //过滤器名称
+////        registration.setName("customFilter");
+////        //拦截路径
+////        registration.addUrlPatterns("/*");
+////        //设置顺序
+////        registration.setOrder(10);
+////        return registration;
+////    }
+////
+////    @Bean
+////    public Filter customFilter() {
+////        return new CustomFilter();
+////    }
 }
