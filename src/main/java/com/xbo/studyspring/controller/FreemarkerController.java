ad2ae11784bf7422c3b40921ec735842c0a3cb7a
bob <wangshyn@gmail.com>
2018-10-15 17:45:03 +0800

    FreeMarker和jsp 参考：https://my.oschina.net/xiedeshou/blog/1923553

---
+++ src/main/java/com/xbo/studyspring/controller/FreemarkerController.java
@@ -0,0 +1,32 @@
+package com.xbo.studyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//因为是返回页面 所以不能是@RestController
+@Controller
+@RequestMapping("/freemarker")
+public class FreemarkerController {
+
+    //正常和springmvc设置返回参数是意义的用法了
+    @GetMapping("/map")
+    public String index(String name,ModelMap map) {
+        map.addAttribute("name", name);
+        map.addAttribute("from", "xbo1.com");
+        //模版名称，实际的目录为：src/main/resources/templates/freemarker.html
+        return "freemarker";
+    }
+
+    @GetMapping("/mv")
+    public ModelAndView index(String name) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", name);
+        mv.addObject("from", "xbo1.com");
+        //模版名称，实际的目录为：src/main/resources/templates/freemarker.html
+        mv.setViewName("freemarker");
+        return mv;
+    }
+}
