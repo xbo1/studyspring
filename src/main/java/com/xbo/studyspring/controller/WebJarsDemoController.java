df0643a72f712a3a005ca4438a46510fe007d9d7
bob <wangshyn@gmail.com>
2018-10-16 11:18:20 +0800

    WebJars使用 参考：https://my.oschina.net/xiedeshou/blog/1924836

---
+++ src/main/java/com/xbo/studyspring/controller/WebJarsDemoController.java
@@ -0,0 +1,13 @@
+package com.xbo.studyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebJarsDemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
